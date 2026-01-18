bb675064d822c85d6c0005e70486a15a7ae8789b
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-01-18 12:57:23 +0100

    refactor Entry functionality

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,11 +1,8 @@
-export function Entry() {
+export function Entry(props) {
   return (
     <article>
       <div className="image-wrapper">
-        <img
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="photo of mount fuji"
-        />
+        <img src={props.image.src} alt={props.image.alt} />
       </div>
       <div className="post-wrapper">
         <ul>
@@ -16,21 +13,15 @@ export function Entry() {
               className="marker-icon"
             />
           </li>
-          <li className="country">Japan</li>
+          <li className="country">{props.country}</li>
           <li>
-            <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-              View on Google Maps
-            </a>
+            <a href={props.googleMapsLink}>View on Google Maps</a>
           </li>
         </ul>
 
-        <h1>Mount Fuji</h1>
-        <span>12 Jan, 2021 - 24 Jan, 2021</span>
-        <p>
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h1>{props.name}</h1>
+        <span>{props.date}</span>
+        <p>{props.description}</p>
       </div>
     </article>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { Entry } from "../components/Entry.jsx";
 import { Header } from "../components/Header.jsx";
+import data from "../data.js";
 
 export function App() {
+  const travelData = data.map((data) => {
+    return (
+      <Entry
+        image={data.img}
+        country={data.country}
+        googleMapsLink={data.googleMapsLink}
+        name={data.title}
+        date={data.dates}
+        description={data.text}
+      />
+    );
+  });
+
   return (
     <>
       <Header />
-      <Entry />
+      {travelData}
     </>
   );
 }
